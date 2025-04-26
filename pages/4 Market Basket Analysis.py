1333c7b7144f383eeb7e7c44449d3f56cc3a330c
Rahulb1331 <109972113+Rahulb1331@users.noreply.github.com>
2025-04-27 03:39:59 +0530

    Update 4 Market Basket Analysis.py

--- pages/4 Market Basket Analysis.py
+++ pages/4 Market Basket Analysis.py
@@ -211,6 +211,8 @@ if not multi_item_txns.empty:
         # --- Top Revenue-Generating Bundles ---
         st.subheader("💰 Top 10 Revenue-Generating Product Bundles")
         top_bundles = rules_df.sort_values('estimated_revenue', ascending=False).head(10)
+
+        #top_bundles = rules_df[['rule', 'support', 'confidence', 'lift', 'estimated_revenue']].head(10)
         st.dataframe(top_bundles.reset_index(drop=True), use_container_width=True)
 
         # --- Strategic Filters (Tactical Sliders) ---
