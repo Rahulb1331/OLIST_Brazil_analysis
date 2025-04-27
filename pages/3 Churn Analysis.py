9959381c73a54d7c9cc1659d43a35bf272cff7a1
Rahulb1331 <109972113+Rahulb1331@users.noreply.github.com>
2025-04-27 17:13:08 +0530

    Update 3 Churn Analysis.py

--- pages/3 Churn Analysis.py
+++ pages/3 Churn Analysis.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
 from sklearn.model_selection import train_test_split, cross_val_score
