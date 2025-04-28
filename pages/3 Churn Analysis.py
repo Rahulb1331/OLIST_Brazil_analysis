68f25b84aec469bddb2bff3153bdcc60fd04346a
Rahulb1331 <109972113+Rahulb1331@users.noreply.github.com>
2025-04-28 16:21:39 +0530

    Update 3 Churn Analysis.py

--- pages/3 Churn Analysis.py
+++ pages/3 Churn Analysis.py
@@ -123,6 +123,8 @@ data["cltv_segment_encoded"] = le.fit_transform(data["CLTV_new_Segment"])
 X = data.drop(columns=["customer_unique_id", "last_order", "first_order", "CLTV_new_Segment", "churned"])
 y = data["churned"]
 
+st.dataframe(data.head(10))
+
 #Correlational heatmap
 
 if st.checkbox("Show feature correlation heatmap"):
